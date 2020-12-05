03e2f3b452cabfc14c7f1493078ae17c8f7f7bd8
Michael Lim <michael@eventxtra.com>
2020-12-06 04:32:38 +0800

    add zoom example

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import styled from 'styled-components';
+import { useState } from 'react';
 
 const Table = styled.div`
   position: absolute;
@@ -64,24 +65,54 @@ const statusColorMap = {
   [NOT_AVAILABLE]: 'black'
 };
 
+function setZoom(zoom,el) {
+      
+  var transformOrigin = [0,0];
+  var p = ["webkit", "moz", "ms", "o"],
+        s = "scale(" + zoom + ")",
+        oString = (transformOrigin[0] * 100) + "% " + (transformOrigin[1] * 100) + "%";
+
+  for (var i = 0; i < p.length; i++) {
+      el.style[p[i] + "Transform"] = s;
+      el.style[p[i] + "TransformOrigin"] = oString;
+  }
+
+  el.style["transform"] = s;
+  el.style["transformOrigin"] = oString;
+  
+}
+
 function App() {
+  const [zoomScale, setZoomScale] = useState(10);
+
+  const showVal = (a) => {
+    var zoomScale = Number(a)/10;
+    setZoomScale(a);
+    setZoom(zoomScale,document.getElementById('container'))
+ } 
+
   return (
-    <div style={{ height: '100%', width: '100%', position: 'relative' }}>
+    <div>
       Select a table
-      {tables.map((table) => (
-        <Table
-          key={table.name}
-          top={table.y_pos}
-          left={table.x_pos}
-          width={table.width}
-          height={table.height}
-          borderRadius={table.radius}
-          onClick={function(e) {
-            e.target.style.backgroundColor = getRandomColor();
-          }}
-          backgroundColor={statusColorMap[table.status]}
-        />
-      ))}
+      <div style={{ width: 600, height: 600, overflow: 'scroll' }}>
+        <div style={{ height: 1200, width: 1200, position: 'relative' }} id="container">
+          {tables.map((table) => (
+            <Table
+              key={table.name}
+              top={table.y_pos}
+              left={table.x_pos}
+              width={table.width}
+              height={table.height}
+              borderRadius={table.radius}
+              onClick={function(e) {
+                e.target.style.backgroundColor = getRandomColor();
+              }}
+              backgroundColor={statusColorMap[table.status]}
+            />
+          ))}
+        </div>
+      </div>
+      <input id="test" min="1" max="10" value={zoomScale} step="0.1" onChange={(e) => showVal(e.target.value)} type="range" style={{ width: 400 }} />
     </div>
   );
 }
